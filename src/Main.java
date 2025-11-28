438ebce71d88b7c24540b782cf96a89c3ad78b78
LukasZavora <blankauhlikova7@gmail.com>
2025-11-28 10:36:01 +0100

    opraveno x2

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,11 @@ public class Main {
         seznam.addFirst(2);
         seznam.addLast(7);
         System.out.println(seznam);
-        System.out.println( seznam.getIndex(3));
+        System.out.println(seznam.getIndex(3));
         System.out.println(seznam.deleteFirst());
         System.out.println(seznam.deleteLast());
         System.out.println(seznam.indexOf(4));
         System.out.println(seznam.contains(10));
-
         System.out.println(seznam);
 
 
